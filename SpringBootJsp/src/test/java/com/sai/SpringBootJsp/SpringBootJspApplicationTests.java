9bfa98ff220741274d12211ed0ea2cfddd79cb3a
Shrikant Kale <kaleshrikant@live.com>
2021-06-22 18:05:48 +0530

    Initial Commit for SpringBootJsp Project

---
+++ SpringBootJsp/src/test/java/com/sai/SpringBootJsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.SpringBootJsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
